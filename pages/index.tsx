fa78dc19412b0507a8043e5d42f4e15996f9e8de
kr-stella <kr_stella@naver.com>
2024-08-18 15:38:46 +0900

    feature/main-swiper 브랜치 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCube, EffectCoverflow, Navigation, Pagination, Autoplay } from "swiper/modules";
 
 const Home = () => {
-	console.log(`develeop`);
+	console.log(`swiper`);
 	return (
 	<main>
 		{/* Swiper */}
