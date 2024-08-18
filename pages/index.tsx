d2f72de5e35bac9fd610028a1f235b7965f0a660
kr-stella <kr_stella@naver.com>
2024-08-18 15:34:57 +0900

    develop 브랜치 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCube, EffectCoverflow, Navigation, Pagination, Autoplay } from "swiper/modules";
 
 const Home = () => {
-
+	console.log(`develeop`);
 	return (
 	<main>
 		{/* Swiper */}
